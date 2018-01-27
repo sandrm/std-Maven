b52fbe5fca2dd425c709a08a0c08251344baa3bd
o m <sandrm@ukr.net>
2018-01-27 17:16:36 +0200

    first submit

---
+++ src/main/java/org/sandrm/studyMaven/App.java
@@ -0,0 +1,12 @@
+package org.sandrm.studyMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Study Maven: Hello World!" );
+    }
+}
